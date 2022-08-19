08ac786c2dcae1f7f6428729ba38cd554efba597
shameoff <eshamov030316@gmail.com>
2022-08-20 06:45:13 +0700

    Little break gateway publisher(need to fix ports maybe), little changes in reciever and publisher because of my attempts to fix

--- marketplace/AGORA_Django/receiver.py
+++ marketplace/AGORA_Django/receiver.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 import pika, sys, os
 
+
 RABBITMQ_USER = os.environ.get("RABBITMQ_USERNAME")
 RABBITMQ_PASSWORD = os.environ.get("RABBITMQ_PASSWORD")
-RABBITMQ_PORT = int(os.environ.get("RABBITMQ_NODE_PORT_NUMBER", 5672))
-RABBITMQ_HOST = os.environ.get("RABBITMQ_NODE_PORT_NUMBER", "localhost")
+RABBITMQ_PORT = int(os.environ.get("RABBITMQ_NODE_PORT_NUMBER", 56721))
+RABBITMQ_HOST = os.environ.get("RABBITMQ_NODE_HOST_NUMBER", "localhost")
+
+# Check env 
+# print(RABBITMQ_USER, RABBITMQ_PASSWORD, RABBITMQ_PORT, RABBITMQ_HOST)
 
 ROUTING_KEY = 'hello'
 
 
 def main():
     credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
-    parameters = pika.ConnectionParameters(RABBITMQ_HOST, RABBITMQ_PORT, '/', credentials)     
+    parameters = pika.ConnectionParameters(RABBITMQ_HOST, RABBITMQ_PORT, '/', credentials=credentials)     
     connection = pika.BlockingConnection(parameters)
     
+
     channel = connection.channel()
     channel.queue_declare(queue=ROUTING_KEY)
 
